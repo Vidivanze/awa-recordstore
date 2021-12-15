0c3f91a080b935d0e6a2e62899fc28cdfcbf26d5
Yvann Butticaz <34337152+Vidivanze@users.noreply.github.com>
2021-12-15 09:58:37 +0100

    Add home page and content component

---
+++ src/components/Content.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="content">
+    <div class="container">coucou</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Content',
+}
+</script>
+
+<style>
+  .content {
+    background: black;
+    height: 100%;
+    width: 100%;
+  }
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <Navbar/>
+  <Content/>
+</template>
+
+<script>
+
+import Navbar from './Navbar.vue'
+import Content from './Content.vue'
+
+export default {
+    name: 'Home',
+
+    components: {
+      Navbar,
+      Content
+    },
+
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<style>
+
+</style>
