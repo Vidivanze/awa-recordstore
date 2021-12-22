b700a9b296c8e0b52898363b54c1de22c3b5660a
Yvann Butticaz <34337152+Vidivanze@users.noreply.github.com>
2021-12-22 10:05:27 +0100

    Create ArrivalCardsList, it's now two lists seperate by genres

---
+++ src/components/LastArrivals/ArrivalCardsList.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="dark:bg-gray-900">
+    <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+      <h1 class="mb-2 text-4xl dark:text-white text-left">Derniers arrivages</h1>
+      <h2 class="mt-5 mb-5 text-3xl text-left dark:text-white">Jazz</h2>
+
+      <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        <template v-for="arrival in arrival_jazz" :key="arrival.id">
+          <div class="group">
+            <arrival-card 
+              :id="arrival.id" 
+              :name="arrival.name" 
+              :artist="arrival.artist"
+              :pressInfos="arrival.pressInfos" 
+              :imageSource="arrival.imageFileName" 
+              :label="arrival.label"
+            ></arrival-card>  
+          </div>
+        </template>
+      </div>
+    </div>
+
+    <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+      <h2 class="mt-4 mb-5 text-3xl text-left dark:text-white">Rap et HipHop</h2>
+
+      <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        <template v-for="arrival in arrival_rap" :key="arrival.id">
+          <div class="group">
+            <arrival-card 
+              :id="arrival.id" 
+              :name="arrival.name" 
+              :artist="arrival.artist"
+              :pressInfos="arrival.pressInfos" 
+              :imageSource="arrival.imageFileName" 
+              :label="arrival.label"
+            ></arrival-card>  
+          </div>
+        </template>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+import ArrivalCard from './ArrivalCard.vue'
+import ArrivalJazz from '../../data/ArrivalJazz.json'
+import ArrivalRap from '../../data/ArrivalRap.json'
+
+
+export default {
+  name: 'ArrivalCardsList',
+
+  components: {
+    ArrivalCard,
+  },
+
+  data() {
+    console.log(ArrivalJazz)
+    console.log(ArrivalRap)
+    return {
+      arrival_jazz: ArrivalJazz,
+      arrival_rap: ArrivalRap,
+    }
+  },
+  
+}
+
+</script>
+
+<style>
+
+</style>
