7ec0a0441463fec08980e9a56e9d91589e849d3f
Yvann Butticaz <34337152+Vidivanze@users.noreply.github.com>
2021-12-23 14:42:18 +0100

    Update cards list, add animation
    Now the card can flip on hover and show info about the product

--- src/components/LastArrivals/ArrivalCard.vue
+++ src/components/LastArrivals/ArrivalCard.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="w-full aspect-w-1 aspect-h-1 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-    <img :src="`/img/products/` + imageSource" class="w-full h-full object-center object-cover group-hover:opacity-75 hover:object-scale-down" />
+    <div class="card">
+      <div class="card-content">
+        <div class="card-front">
+          <img :src="`/img/products/` + genre + '/' + imageSource" class="w-full h-full object-center object-cover group-hover:opacity-75 hover:object-scale-down" />
+        </div>
+        <div class="card-back">
+          <strong class="card-text">{{ artist }}</strong>
+          <h2 class="card-text">{{ label }}</h2>
+          <h2 class="card-text">{{ pressInfos }}</h2>
+        </div>
+      </div>
+    </div>
   </div>
-  <h3 class="mt-4 text-lg text-white">
+  <h3 class="mt-4 text-lg card-title">
     {{ name }}
   </h3>
-  <h3 class="mt-1 text-base text-white">
-    {{ artist }}
-  </h3>
-  <p class="mt-1 text-sm font-medium text-white">
-    {{ pressInfos }}
-  </p>
 </template>
 
 <script>
@@ -19,10 +24,11 @@ export default {
   name: 'ArrivalCard',
   props: { 
     name: String,
+    genre: String,
     artist: String,
     pressInfos: Number,
     imageSource: String,
-    imageAlt: String,
+    label: String,
   },
 
   setup(){
@@ -31,10 +37,151 @@ export default {
       image,
     }
   },
-
 }
+
 </script>
 
 <style>
+  .card{
+    perspective: 600px; 
+    text-align: center;
+  }
+  .card-content {   
+    position: relative;
+    object-fit: contain;
+    min-width: 100%;
+    min-height: 247px;
+    width: 100%;
+    height: 100%;
+    transition: transform 0.4s;
+    transform-style: preserve-3d;
+  }
+
+  .card-title {
+    color: #BBE1FA;
+  }
+
+  .card-text {
+    color: #fff;
+  }
+
+  .card:hover .card-content{
+    transform: rotateY(180deg);
+    transition: transform 0.3s;
+  }
+
+  .card-front {
+    position: absolute;
+    width: 100%;
+    backface-visibility: hidden;  
+    color: #fff;  
+  }
+
+  .card-back {
+    width: 100%;
+    height: 100%;
+    text-align: center;
+    line-height: 60px;
+    backface-visibility: hidden;  
+    color: #fff;  
+    background-color: #0F4C75;  
+    transform: rotateY(180deg);
+    border-right: 6px solid #3282B8;
+    border-bottom: 6px solid #3282B8;
+    box-shadow: 6px 6px #3282B8;
+  }
+
+
+  /*Media querries */
+
+   @media screen and (min-width: 1400px) {
+    .card-content, .card-back {
+      min-height: 218px;
+    }
+  }
+
+  @media screen and (min-width: 1300px) {
+    .card-content, .card-back {
+      min-height: 218px;
+    }
+  }
+
+  @media screen and (max-width: 1298px) {
+    .card-content, .card-back {
+      min-height: 214px;
+    }
+  }
+
+  @media screen and (max-width: 1279px) {
+    .card-content, .card-back {
+      min-height: 265px;
+    }
+  }
+  
+  @media screen and (max-width: 1200px) {
+    .card-content, .card-back {
+      min-height: 244px;
+    }
+  }
+
+  @media screen and (max-width: 1199px) {
+    .card-content, .card-back {
+      min-height: 250px;
+    }
+  }
+
+  @media screen and (max-width: 1150px) {
+    .card-content, .card-back {
+      min-height: 251px;
+    }
+  }
+
+  @media screen and (max-width: 1100px) {
+    .card-content, .card-back {
+      min-height: 220px;
+    }
+  }
+
+  @media screen and (max-width: 1079px) {
+    .card-content, .card-back {
+      min-height: 218px;
+    }
+  }
+
+  @media screen and (max-width: 1055px) {
+    .card-content, .card-back {
+      min-height: 228px;
+    }
+  }
+
+  @media screen and (max-width: 1022px) {
+    .card-content, .card-back {
+      min-height: 300px;
+    }
+  }
+
+  @media screen and (max-width: 800px) {
+    .card-content, .card-back {
+      min-height: 296px;
+    }
+  }
+
+  @media screen and (max-width: 639px) {
+    .card-content, .card-back {
+      min-height: 512px;
+    }
+  }
+
+   @media screen and (max-width: 520px) {
+    .card-content, .card-back {
+      min-height: 474px;
+    }
+  }
+
+  @media screen and (max-width: 450px) {
+    .card-content, .card-back {
+      min-height: 416px;
+    }
+  }
 
 </style>
--- src/components/LastArrivals/ArrivalCardsList.vue
+++ src/components/LastArrivals/ArrivalCardsList.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="dark:bg-gray-900">
+  <div class="cards-list">
     <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-      <h1 class="mb-2 text-4xl dark:text-white text-left">Derniers arrivages</h1>
-      <h2 class="mt-5 mb-5 text-3xl text-left dark:text-white">Jazz</h2>
 
-      <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+      <h1 class="mb-2 text-4xl text-left title">Derniers arrivages</h1>
+      <h2 class="mt-5 mb-5 text-3xl text-left title">Jazz</h2>
+
+      <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-4 xl:grid-cols-5 xl:gap-x-8">
         <template v-for="arrival in arrival_jazz" :key="arrival.id">
           <div class="group">
             <arrival-card 
               :id="arrival.id" 
+              genre="jazz"
               :name="arrival.name" 
               :artist="arrival.artist"
               :pressInfos="arrival.pressInfos" 
@@ -21,13 +23,14 @@
     </div>
 
     <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-      <h2 class="mt-4 mb-5 text-3xl text-left dark:text-white">Rap et HipHop</h2>
+      <h2 class="mt-4 mb-5 text-3xl text-left title">Rap et HipHop</h2>
 
-      <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+      <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-4 xl:grid-cols-5 xl:gap-x-8">
         <template v-for="arrival in arrival_rap" :key="arrival.id">
           <div class="group">
             <arrival-card 
               :id="arrival.id" 
+              genre="rap"
               :name="arrival.name" 
               :artist="arrival.artist"
               :pressInfos="arrival.pressInfos" 
@@ -70,5 +73,10 @@ export default {
 </script>
 
 <style>
-
+  .title{
+    color: #BBE1FA;
+  }
+  .cards-list{
+    background-color: #1B262C;
+  }
 </style>
