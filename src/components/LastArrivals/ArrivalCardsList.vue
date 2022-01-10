73299662564363581e50912505a72be7c79d357d
Yvann Butticaz <34337152+Vidivanze@users.noreply.github.com>
2022-01-10 15:58:31 +0100

    Make card list more dynamic

--- src/components/LastArrivals/ArrivalCardsList.vue
+++ src/components/LastArrivals/ArrivalCardsList.vue
@@ -1,52 +1,23 @@
 <template>
-  <div class="cards-list">
-    <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
 
-      <h1 class="mb-2 text-4xl text-left title">Derniers arrivages</h1>
-      <h2 class="mt-5 mb-5 text-3xl text-left title">Jazz</h2>
-
-      <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-4 xl:grid-cols-5 xl:gap-x-8">
-        <template v-for="arrival in arrival_jazz" :key="arrival.id">
-          <div class="group">
-            <arrival-card 
-              :id="arrival.id" 
-              genre="jazz"
-              :name="arrival.name" 
-              :artist="arrival.artist"
-              :pressInfos="arrival.pressInfos" 
-              :imageSource="arrival.imageFileName" 
-              :label="arrival.label"
-            ></arrival-card>  
-          </div>
-        </template>
+  <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-4 xl:grid-cols-5 xl:gap-x-8">
+    <template v-for="arrival in arrival_list" :key="arrival.id">
+      <div class="group">
+        <arrival-card 
+          :id="arrival.id" 
+          :genre="genre"
+          :name="arrival.name" 
+          :artist="arrival.artist"
+          :pressInfos="arrival.pressInfos" 
+          :imageSource="arrival.imageFileName" 
+          :label="arrival.label"
+        ></arrival-card>  
       </div>
-    </div>
-
-    <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-      <h2 class="mt-4 mb-5 text-3xl text-left title">Rap et HipHop</h2>
-
-      <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-4 xl:grid-cols-5 xl:gap-x-8">
-        <template v-for="arrival in arrival_rap" :key="arrival.id">
-          <div class="group">
-            <arrival-card 
-              :id="arrival.id" 
-              genre="rap"
-              :name="arrival.name" 
-              :artist="arrival.artist"
-              :pressInfos="arrival.pressInfos" 
-              :imageSource="arrival.imageFileName" 
-              :label="arrival.label"
-            ></arrival-card>  
-          </div>
-        </template>
-      </div>
-    </div>
-
+    </template>
   </div>
 </template>
 
 <script>
-
 import ArrivalCard from './ArrivalCard.vue'
 import ArrivalJazz from '../../data/ArrivalJazz.json'
 import ArrivalRap from '../../data/ArrivalRap.json'
@@ -55,26 +26,29 @@ import ArrivalRap from '../../data/ArrivalRap.json'
 export default {
   name: 'ArrivalCardsList',
 
+  props: {
+    genre: String
+  },
+
   components: {
     ArrivalCard,
   },
 
-  data() {
-    return {
-      arrival_jazz: ArrivalJazz,
-      arrival_rap: ArrivalRap,
+  data(props) {
+    if(props.genre == "Jazz"){
+      console.log("tto")
+      return {
+        arrival_list: ArrivalJazz,
+      }
+    }
+    
+    if(props.genre == "Rap"){
+      return {
+        arrival_list: ArrivalRap,
+      }
     }
   },
-  
 }
 
 </script>
 
-<style>
-  .title{
-    color: #BBE1FA;
-  }
-  .cards-list{
-    background-color: #1B262C;
-  }
-</style>
