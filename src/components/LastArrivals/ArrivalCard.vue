9531c8517f8266d5907f25bf255f1f3bb3858029
Yvann Butticaz <34337152+Vidivanze@users.noreply.github.com>
2023-03-01 15:43:00 +0100

    Remove debug code

--- src/components/LastArrivals/ArrivalCard.vue
+++ src/components/LastArrivals/ArrivalCard.vue
@@ -23,7 +23,7 @@
   <h3 class="mt-4 text-lg card-title">
     {{ name }}
   </h3>
-  {{ filePath }}
+  
   <!-- use the modal component, pass in the prop -->
   <transition name="modal">
   <modal v-bind="$attrs" v-if="showModal" @close="showModal = false">
